a7c747c4ea11ac8389724892dc1415419fc56ae3
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-07-17 22:17:21 -0600

    Agregar archivos de proyecto.

---
+++ ClaseQ32024/Ave.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ClaseQ32024
+{
+    public class Ave
+    {
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public FamiliaAve FamiliaAve { get; set; }
+        public string SexoBiologico { get; set; }
+        public bool EsVoladora { get; set; }
+
+
+        //Constructor 
+        public Ave()
+        {
+            EsVoladora = true;
+            FamiliaAve = new FamiliaAve();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Size}");
+            Console.WriteLine($"Nombre de Familia: {FamiliaAve.Nombre}");
+            Console.WriteLine($"Nombre de Grupo: {FamiliaAve.Grupo}");
+            Console.WriteLine($"Sexo Biológico: {SexoBiologico}");
+            Console.WriteLine($"Es Voladora: {(EsVoladora ? "Si" : "No")}");
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ ClaseQ32024/Program.cs
@@ -0,0 +1,26 @@
+﻿//Instanciar la clase o declarar un objeto
+using ClaseQ32024;
+
+Ave ave = new Ave();
+ave.Nombre = "Pájaro";
+ave.Color = "Rojo";
+ave.Size = "Grande";
+ave.FamiliaAve.Nombre = "El pájaro";
+ave.FamiliaAve.Grupo = "Carpintero";
+ave.SexoBiologico = "Macho";
+ave.EsVoladora = true;
+ave.Imprimir();
+
+
+
+
+Ave ave2 = new Ave();
+ave2.Nombre = "Pájaro";
+ave2.Color = "Verde";
+ave2.Size = "Grande";
+ave2.FamiliaAve.Nombre = "El pájaro";
+ave2.FamiliaAve.Grupo = "Loco";
+ave2.SexoBiologico = "Femenino";
+ave2.EsVoladora = false;
+ave2.Imprimir();
+
